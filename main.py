7ad07ecb4e6ff84e862d739ae4c0f6715b40066a
feeeelx <felixnavidad1337@gmail.com>
2025-02-10 11:33:43 -0500

    jkn

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def ClickOnClicker():
     tt.pack_forget()
     tt = ctk.CTkLabel(root, text=hawktuah)
     tt.pack()
-
+3
 clicker = ctk.CTkButton(root, text="my button", command=ClickOnClicker)
 clicker.pack(padx=20, pady=20)
 
