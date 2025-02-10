bfff4a82339c4efe2ffe511a141ecc4bb2461bdf
feeeelx <felixnavidad1337@gmail.com>
2025-02-10 11:24:29 -0500

    asfd

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 import customtkinter as ctk
-
-
+hawktuah = 1
 
 root = ctk.CTk()
 root.geometry("400x150")
 
 
-
 def ClickOnClicker():
-    print('dd')
+    global hawktuah
+    global tt
+    hawktuah = hawktuah + 1
+    print(hawktuah)
+    tt.pack_forget()
+    tt = ctk.CTkLabel(root, text=hawktuah)
+    tt.pack()
 
 clicker = ctk.CTkButton(root, text="my button", command=ClickOnClicker)
 clicker.pack(padx=20, pady=20)
 
+tt = ctk.CTkLabel(root, text=hawktuah)
+tt.pack()
 
 
-
-
-
-
-
-
-
-
+root.update()
 root.mainloop()
