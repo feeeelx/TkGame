30f99a5bb4a02c50bd46e40c6096da92e77a3358
feeeelx <felixnavidad1337@gmail.com>
2025-02-10 11:58:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,5 @@
-import customtkinter as ctk
-hawktuah = 1
+import tkinter as tk
 
-root = ctk.CTk()
-root.geometry("400x150")
-
-
-def ClickOnClicker():
-    global hawktuah
-    global tt
-    hawktuah = hawktuah + 1
-    print(hawktuah)
-    tt.pack_forget()
-    tt = ctk.CTkLabel(root, text=hawktuah)
-    tt.pack()
-3
-clicker = ctk.CTkButton(root, text="my button", command=ClickOnClicker)
-clicker.pack(padx=20, pady=20)
-
-tt = ctk.CTkLabel(root, text=hawktuah)
-tt.pack()
-
-
-root.update()
-root.mainloop()
+GameChoiceWindow = tk.Toplevel()
+GameChoiceWindow.mainloop()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+                                                                                                                                                
