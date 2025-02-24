b3d25a0c216d4e7ec758b7db4daeec48647a921a
feeeelx <felixnavidad1337@gmail.com>
2025-02-24 11:45:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import tkinter as tk
 import os
+import random
 
 FolderDir = "C:\\Users\\felix\\Downloads\\d"
+stringvar = "test"
+testlist = ['a', 'b', 'c', 'd']
 
 def MakeFolders():
+    wordintestlist = testlist[random.randrange(0,3)]
     os.makedirs(FolderDir + '\\ok', exist_ok=True)
+    os.makedirs(FolderDir + '\\ok' + wordintestlist, exist_ok=True)
 
 
 root = tk.Tk()
-button = tk.Button(root, text='ttt', command=MakeFolders)
+button = tk.Button(root, text='Make Folders', command=MakeFolders)
 button.pack()
 
 
